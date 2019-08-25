675d90a83c8f2686ea7cb094a8aaa66fc7ca6c6f
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2019-08-25 13:20:47 +0200

    Add 'meow' command

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from random import choice
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+import requests
 
 from get_facts import get_facts
 
@@ -17,6 +18,11 @@ bot = commands.Bot(command_prefix='!')
 @bot.command()
 async def cat(ctx):
     await ctx.send(choice(facts))
+
+@bot.command()
+async def meow(ctx):
+    json_data = requests.get('https://api.thecatapi.com/v1/images/search').json()
+    await ctx.send(json_data[0]['url'])
 
 print('Running the bot...')
 bot.run(API_KEY)
